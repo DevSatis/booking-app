7c1911ade0359e14f919766031fe7a9881e5b32e
DevSatis <sksatishk9@gmail.com>
2022-08-20 11:26:53 +0530

    Featured_changes2

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -14,21 +14,21 @@ const Featured = () => {
         <div className='featuredItem'>
             <img src="https://images.unsplash.com/photo-1563396984197-4741699a9415?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80" alt="" className='featuredImg'/>
             <div className='featuredTitles'>
-                <h1>Dublin</h1>
+                <h1>California</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className='featuredItem'>
             <img src="https://images.unsplash.com/photo-1535068484622-7a077e5aa558?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" className='featuredImg'/>
             <div className='featuredTitles'>
-                <h1>Dublin</h1>
+                <h1>Nainital</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className='featuredItem'>
-            <img src="https://images.unsplash.com/photo-1611616910032-b63f95736a93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" className='featuredImg'/>
+            <img src="https://images.unsplash.com/photo-1535463731090-e34f4b5098c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="" className='featuredImg'/>
             <div className='featuredTitles'>
-                <h1>Dublin</h1>
+                <h1>Hrishikesh</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
