0550dcdae8260514bbb19ff817901ee40ab5d79d
DevSatis <sksatishk9@gmail.com>
2022-08-26 13:57:24 +0530

    Search List Commit

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,44 +1,93 @@
-import React, { useState } from 'react';
-import './List.css';
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navbar/Navbar';
-import {useLocation} from 'react-router-dom';
-import { format } from 'date-fns';
+import React, { useState } from "react";
+import "./List.css";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
-
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
   return (
     <div>
       <Navbar />
-      <Header type="list"/>
-      <div className='listContainer'>
-        <div className='listWrapper'>
-          <div className='listSearch'>
-            <h1 className='lsTitle'>Search</h1>
-            <div className='lsItem'>
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
               <lebel>Destination</lebel>
-              <input type='text' placeholder={destination}/>
+              <input type="text" placeholder={destination} />
             </div>
-            <div className='lsItem'>
+            <div className="lsItem">
               <lebel>Check in date</lebel>
-              <span>{`
+              <span onClick={() => setOpenDate(!openDate)}>{`
                 ${format(date[0].startDate, "MM/dd/yyyy")} to
-                ${format(date[0].endDate, "MM/dd/yyyy")}`
-
-              }</span>
-              <input type='text' />
+                ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
             </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min Price <small>per night</small>
+                  </span>
+                  <input type="number" min={0} className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max Price <small>per night</small>
+                  </span>
+                  <input type="number" min={0} className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.room}
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
           </div>
-          <div className='listResult'></div>
+          <div className="listResult"></div>
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default List
+export default List;
