ee6df694f8b53a02e4b468ca52f8dbaac83d8409
DevSatis <sksatishk9@gmail.com>
2022-08-29 17:54:37 +0530

    Hotel Page

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -6,29 +6,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 const Hotel = () => {
-
   const photos = [
     {
-      src:"https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    },
     {
-      src:"https://images.unsplash.com/photo-1585418694458-dc80a5c20294?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1585418694458-dc80a5c20294?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    },
     {
-      src:"https://images.unsplash.com/photo-1611048267451-e6ed903d4a38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1611048267451-e6ed903d4a38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+    },
     {
-      src:"https://images.unsplash.com/photo-1587702068694-a909ef4aa346?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1587702068694-a909ef4aa346?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
     {
-      src:"https://images.unsplash.com/photo-1583953458882-302655b5c376?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1583953458882-302655b5c376?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    },
     {
-      src:"https://images.unsplash.com/photo-1546967900-1bea5f16b69d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
-  },
+      src: "https://images.unsplash.com/photo-1546967900-1bea5f16b69d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
   ];
-
-
 
   return (
     <div>
@@ -48,9 +45,9 @@ const Hotel = () => {
             Book a stay over $114 at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
-            {photos.map(photo =>(
+            {photos.map((photo) => (
               <div className="hotelImageWrapper">
-                <img src={photo.src} alt="" className="hotelImg"/>
+                <img src={photo.src} alt="" className="hotelImg" />
               </div>
             ))}
           </div>
@@ -58,24 +55,23 @@ const Hotel = () => {
             <div className="hotelDetailsTexts">
               <h1 className="hotelTitle">Stay in the heart of Bangalore</h1>
               <p className="hotelDesc">
-              The 272 well-appointed rooms include 22 suites. 
-              All units have attached bathroom with a power shower to 
-              recharge you after a long and busy day. 
-              Some units include a seating area for your convenience. 
-              For your comfort, you will find slippers and free toiletries.
-              Located 34 kms from Kempegowda International Airport, 
-              the hotel has easy access to Bengaluru City Junction Railway Station, 
-              Bus Station, and Kempegowda Metro Station.
+                The 272 well-appointed rooms include 22 suites. All units have
+                attached bathroom with a power shower to recharge you after a
+                long and busy day. Some units include a seating area for your
+                convenience. For your comfort, you will find slippers and free
+                toiletries. Located 34 kms from Kempegowda International
+                Airport, the hotel has easy access to Bengaluru City Junction
+                Railway Station, Bus Station, and Kempegowda Metro Station.
               </p>
             </div>
             <div className="hotelDetailsPrice">
               <h1>Perfect for 9-night stay!</h1>
               <span>
-                Located in the real heart of the Bangalore, this property has an excellent 
-                location score is 9.8! 
+                Located in the real heart of the Bangalore, this property has an
+                excellent location score is 9.8!
               </span>
               <h2>
-                <b>$250</b> (9-nights) 
+                <b>$250</b> (9-nights)
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
